2e42826da3cab78301c13910bedc01fac113bd25
pricedown <josephmisaacs@gmail.com>
2024-09-27 09:30:16 -0400

    Texture change to bees

--- assignments/assignment2/main.cpp
+++ assignments/assignment2/main.cpp
@@ -20,8 +20,8 @@ const float bgVertices[] = {
 	// position			 // color				  // texture coords
 	-1.0f, -1.0f, 0.0f,	 0.0f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // Bottom left
 	 1.0f, -1.0f, 0.0f,	 1.0f, 0.0f, 0.0f, 1.0f,  3.0f, 0.0f, // Bottom right
-	 1.0f,  1.0f, 0.0f,	 1.0f, 1.0f, 0.0f, 1.0f,  3.0f, 3.0f, // Top right
-	-1.0f,  1.0f, 0.0f,	 0.0f, 1.0f, 0.0f, 1.0f,  0.0f, 3.0f, // Top left
+	 1.0f,  1.0f, 0.0f,	 1.0f, 1.0f, 0.0f, 1.0f,  3.0f, 2.531f, // Top right
+	-1.0f,  1.0f, 0.0f,	 0.0f, 1.0f, 0.0f, 1.0f,  0.0f, 2.531f, // Top left
 };
 
 const float guyVertices[] = {
