062cd65edde10454065d503e8f758f52d2dbd82c
pricedown <josephmisaacs@gmail.com>
2024-10-21 16:21:26 -0400

    Many cubes

--- assignments/assignment4/assets/shaders/shader.vert
+++ assignments/assignment4/assets/shaders/shader.vert
@@ -7,14 +7,12 @@ layout (location = 1) in vec2 aTexCoord;
 out vec2 TexCoord;
 out vec4 vColor;
 
-uniform mat4 transform;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-	//gl_Position = projection * view * model * vec4(aPos,1.0);
-	gl_Position = transform * vec4(aPos,1.0);
+	gl_Position = projection * view * model * vec4(aPos,1.0);
 	TexCoord = aTexCoord;
 	//vColor = aColor;
 }
--- assignments/assignment4/main.cpp
+++ assignments/assignment4/main.cpp
@@ -74,6 +74,18 @@ const unsigned int cubeIndices[] = {
 	2, 3, 0, 
 };
 
+glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+};
 
 int main() {
 	#pragma region Initialization
@@ -155,14 +167,20 @@ int main() {
 		glClear(GL_DEPTH_BUFFER_BIT);
 
 		shader.use();
-		shader.setMat4("transform", projection * view * model);
 		shader.setMat4("projection", projection);
 		shader.setMat4("view", view);
-		shader.setMat4("model", model);
 		brick.Bind(GL_TEXTURE0);
-		
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glBindVertexArray(VAO);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glfwSwapBuffers(window);
 	}
