7d6b5523e71b4cb51b0251242a41181d2ea998c8
pricedown <josephmisaacs@gmail.com>
2024-10-21 19:35:55 -0400

    Clarified assignment4

--- assignments/assignment4/assets/shaders/shader.vert
+++ assignments/assignment4/assets/shaders/shader.vert
@@ -1,7 +1,6 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
-//layout (location = 1) in vec4 aColor;
 layout (location = 1) in vec2 aTexCoord;
 
 out vec2 TexCoord;
@@ -14,5 +13,4 @@ uniform mat4 projection;
 void main() {
 	gl_Position = projection * view * model * vec4(aPos,1.0);
 	TexCoord = aTexCoord;
-	//vColor = aColor;
 }
--- assignments/assignment4/main.cpp
+++ assignments/assignment4/main.cpp
@@ -23,25 +23,19 @@ enum ProjectionType {
 	ORTHOGRAPHIC = 0,
 	PERSPECTIVE = 1
 };
-
+ 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-const glm::vec3 UP = glm::vec3(0.0f, 1.0f, 0.0f);
-
-ProjectionType cameraMode = ProjectionType::PERSPECTIVE;
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f); //- Camera starts looking looking along world Z axis.
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f); 
+const glm::vec3 UP = glm::vec3(0.0f, 1.0f, 0.0f);
+const float nearPlane = 0.1f, farPlane = 1000.0f; //- Near plane of 0.1, Far plane of 1000
 
-float pitch, yaw;
-float nearPlane = 0.1f, farPlane = 1000.0f; //- Near plane of 0.1, Far plane of 1000
-const float walkSpeed = 2.5f, runSpeed = 5.0f;
-
-float lastX = 0, lastY = 0;
-const float sensitivity = 0.1f;
-float fov = 60.0f; //- Perspective projection with a default FOV of 60 degrees
-bool firstMouse = true;
-
+const unsigned int CUBENUM = 20;
+glm::vec3 cubePositions[CUBENUM];
+glm::vec3 cubeRotationDirections[CUBENUM];
+glm::vec3 cubeScales[CUBENUM];
 const float cubeVertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -86,23 +80,13 @@ const float cubeVertices[] = {
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-const unsigned int cubeIndices[] = {
-	0, 1, 2, 
-	2, 3, 0, 
-};
-
-const unsigned int CUBENUM = 20;
-glm::vec3 cubePositions[CUBENUM];
-glm::vec3 cubeRotationDirections[CUBENUM];
-
+const float speed = 2.5f, sprintSpeedMultiplier = 2.0f;
 void movementInput(GLFWwindow* window) {
-	float cameraSpeed = deltaTime; //- All movement must be scaled by deltaTime to be framerate independent
+	float cameraSpeed = speed * deltaTime; //- All movement must be scaled by deltaTime to be framerate independent
 	
 	//- Left Shift to Sprint(double base movement speed if held)*
 	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) 
-		cameraSpeed *= 5.0f;
-	else
-		cameraSpeed *= 2.5f;
+		cameraSpeed *= sprintSpeedMultiplier;
 
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 		cameraPos += cameraSpeed * cameraFront;
@@ -120,25 +104,33 @@ void movementInput(GLFWwindow* window) {
 		cameraPos += glm::normalize(glm::cross(glm::cross(cameraFront, UP), cameraFront)) * cameraSpeed;
 }
 
-bool holding = false;
-void processInput(GLFWwindow* window)
-{
-	movementInput(window);
-	
-	//- Bonus +2: Add an option to toggle between Perspective and Orthographic projection.
-	// [TAB] to change mode
+//- Bonus +2: Add an option to toggle between Perspective and Orthographic projection.
+// press [TAB] 
+ProjectionType cameraMode = ProjectionType::PERSPECTIVE;
+bool holdingCameraModeToggle = false;
+void cameraProjectionInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS) {
-		if (holding == false) {
+		if (holdingCameraModeToggle == false) {
 			cameraMode = (ProjectionType)((cameraMode + 1) % 2);
 		}
-		holding = true;
+		holdingCameraModeToggle = true;
 	}
 	else {
-		holding = false;
+		holdingCameraModeToggle = false;
 	}
 }
 
+void processInput(GLFWwindow* window)
+{
+	movementInput(window);
+	cameraProjectionInput(window);
+}
+
 //- Aiming with mouse
+const float sensitivity = 0.1f;
+float pitch, yaw = -90.0f; //- Camera starts looking looking along world Z axis.
+float lastX = 0.0f, lastY = 0.0f;
+bool firstMouse = true;
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 	if (firstMouse)
 	{
@@ -152,7 +144,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 	lastX = xpos;
 	lastY = ypos;
 
-	float sensitivity = 0.1f;
 	xoffset *= sensitivity;
 	yoffset *= sensitivity;
 
@@ -173,6 +164,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 }
 
 //- Mouse scroll to change FOV
+float fov = 60.0f; //- Perspective projection with a default FOV of 60 degrees
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
 	fov -= (float)yoffset;
@@ -188,7 +180,6 @@ int main() {
 	#pragma region Initialization
 	printf("Initializing...");
 	GLFWwindow* window;
-	// glfw.org, a library separate from opengl captures inputs and puts onto screen
 	if (!glfwInit()) {
 		printf("GLFW failed to init!");
 		return 1;
@@ -198,8 +189,7 @@ int main() {
 		printf("GLFW failed to create window");
 		return 1;
 	}
-	glfwMakeContextCurrent(window); // this is what we're rendering to
-	// glad literally just identifies the headers for interacting with our machine
+	glfwMakeContextCurrent(window);
 	if (!gladLoadGL(glfwGetProcAddress)) {
 		printf("GLAD Failed to load GL headers");
 		return 1;
@@ -216,25 +206,25 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
-
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	//glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(sizeof(float) * 3));
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(sizeof(float) * 3));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
-	//glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	#pragma endregion
-
+	
+	// Cubes random positions, rotations, and scales
 	for (int i = 0; i < CUBENUM; i++) {
-		float maxDist = 5.f;
-		cubePositions[i].x = ew::RandomRange(-maxDist, maxDist);
-		cubePositions[i].y = ew::RandomRange(-maxDist, maxDist);
-		cubePositions[i].z = ew::RandomRange(-maxDist, maxDist);
+		float cubeBounds = 6.0f;
+		float fieldStartsAt = -4.0f;
+		cubePositions[i].x = ew::RandomRange(-cubeBounds, cubeBounds);
+		cubePositions[i].y = ew::RandomRange(-cubeBounds, cubeBounds);
+		cubePositions[i].z = ew::RandomRange(fieldStartsAt, fieldStartsAt-cubeBounds*2);
+		
+		float cubeMinScale = 0.25f;
+		float cubeMaxScale = 1.2f;
+		cubeScales[i] = glm::vec3(ew::RandomRange(cubeMinScale, cubeMaxScale));
 		cubeRotationDirections[i] = glm::vec3(ew::RandomRange(-1.0f, 1.0f), ew::RandomRange(-1.0f, 1.0f), ew::RandomRange(-1.0f, 1.0f));
 	}
 
@@ -255,42 +245,39 @@ int main() {
 		deltaTime = time - lastFrame;
 		lastFrame = time;
 
-		glm::mat4 model = glm::mat4(1.0);
-
-		glm::mat4 view;
-		view = glm::lookAt(cameraPos, cameraPos + cameraFront, UP);
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, UP); 
 
 		glm::mat4 projection = glm::mat4(1.0);
-		if (cameraMode == ProjectionType::PERSPECTIVE) {
-			projection = glm::perspective(glm::radians(fov), (float)SCREEN_WIDTH/SCREEN_HEIGHT, nearPlane, farPlane);  // FOV accounts for the screen's aspect ratio
-		}
-		else if (cameraMode == ProjectionType::ORTHOGRAPHIC) {
-			projection = glm::ortho(-SCREEN_WIDTH / 400.0f, SCREEN_WIDTH / 400.0f, -SCREEN_HEIGHT / 400.0f, SCREEN_HEIGHT / 400.0f, nearPlane, farPlane);
-		}
+		if (cameraMode == ProjectionType::PERSPECTIVE)
+			projection = glm::perspective(glm::radians(fov), (float)SCREEN_WIDTH/SCREEN_HEIGHT, nearPlane, farPlane);  //- FOV accounts for the screen's aspect ratio
+		else 
+			projection = glm::ortho(-SCREEN_WIDTH / 300.0f, SCREEN_WIDTH / 300.0f, -SCREEN_HEIGHT / 300.0f, SCREEN_HEIGHT / 300.0f, nearPlane, farPlane);
 		
 		// Draw
 
 		// background
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.2f, 0.2f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		// brick square
-		glEnable(GL_BLEND);
+		// brick cubes
 		glEnable(GL_DEPTH_TEST);
 		glClear(GL_DEPTH_BUFFER_BIT);
 
 		shader.use();
+		brick.Bind(GL_TEXTURE0);
+		glBindVertexArray(VAO);
+
 		shader.setMat4("projection", projection);
 		shader.setMat4("view", view);
-		brick.Bind(GL_TEXTURE0);
-
-		glBindVertexArray(VAO);
 		for (unsigned int i = 0; i < CUBENUM; i++)
 		{
 			glm::mat4 model = glm::mat4(1.0f);
+
 			model = glm::translate(model, cubePositions[i]);
-			float angle = 7.0f * (i+1) * time;
+			float angle = 7.0f * (i+1) * time; // BONUS +2: Animate your cubes by changing position, rotation, and/or scale every frame. This movement must be scaled with deltaTime.
 			model = glm::rotate(model, glm::radians(angle), cubeRotationDirections[i]);
+			model = glm::scale(model, cubeScales[i]);
+
 			shader.setMat4("model", model);
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
